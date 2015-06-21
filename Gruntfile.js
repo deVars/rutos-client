0e3e17f670a6f23deefb56767ec684d35d6b4321
Roseller M. Velicaria, Jr <r.velicaria.jr@gmail.com>
2015-06-20 18:02:46 -0700

    renamed Gruntfile to Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,6 +63,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	
-
+	grunt.registerTask('default', ['htmlmin', 'cssmin', 'uglify']);
 };
